eee332a379f4bee6db461b5ff497c52f9c5536f6
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-08-18 08:54:45 -0700

    App is now working save for one db connection bug upon refreshing the page without restarting the server

--- routes/login.js
+++ routes/login.js
@@ -7,25 +7,29 @@ const saltRounds = 10;
 const cookieSession = require('cookie-session');
 
 loginRouter.get('/login', (request, response) => {
+  let errorObject = {
+    error: false,
+    message: ""
+  };
+  // If you are logged in, redirect to the homepage
+  if (request.session.email) {
+    response.redirect('/');
+  }
   // If no query error, render the page
   if (!request.query.err) {
-    response.render('login', {
-      error: false
-    });
+    response.render('login', errorObject);
   }
   // If the first or second error handling occurred,
   // render the page plus the appropriate message
   else if (request.query.err === 'err1') {
-    response.render('login', {
-      error: true,
-      message: 'Please provide an email and a password to login.'
-    })
+    errorObject.error = true;
+    errorObject.message = 'Please provide an email and a password to login.';
+    response.render('login', errorObject);
   }
   else if (request.query.err === 'err2') {
-    response.render('login', {
-      error: true,
-      message: 'The email or password was incorrect. Please try again.'
-    })
+    errorObject.error = true;
+    errorObject.message = 'Your email or password was incorrect, please try again.';
+    response.render('login', errorObject);
   }
 });
 
@@ -38,45 +42,31 @@ loginRouter.post('/login', (request, response, next) => {
   the email and password combination provided match what's in the database
   */
   else{
-    let databaseEmailPassword = [];
-    let submittedEmail = request.body.email;
-    let submittedPassword = request.body.password;
-    let queryText = `SELECT email, password FROM users WHERE email = '${submittedEmail}'`;
+    let databasePassword = [];
+    let queryText = `SELECT email, password FROM users WHERE email = '${request.body.email}'`;
     client.query(queryText)
     .then(result => {
       result.rows.map(formInput => {
-        databaseEmailPassword.push(formInput.email);
-        databaseEmailPassword.push(formInput.password);
-        client.end();
+        databasePassword.push(formInput.password)
       })
     })
-    .catch(error => response.redirect('/login/?err=err2'))
-    .then(result => bcrypt.compare(submittedPassword, databaseEmailPassword[1]))
+    .then(result => bcrypt.compare(request.body.password, databasePassword[0]))
     .then(result => {
-      if (true) {
+      if (result === true) {
         next();
       }
       else {
         response.redirect('/login/?err=err2');
       }
     })
-    .catch(error => {
-      console.error(error.stack);
-    })
+    .catch(error => response.redirect(console.error(error)))
   }
 });
-
-// set cookie-session
-
-loginRouter.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2', 'key3', 'key4']
-}));
 
 // redirect to homepage with cookie now set, but first set the session to use
 
 loginRouter.post('/login', (request, response, next) => {
-  request.session.email = response.cookie;
+  request.session.email = request.body.email;
   response.redirect('/');
 })
 
